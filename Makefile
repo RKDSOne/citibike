ba7df7b3fbb94d4675d05b6383e3c5a59f434a51
Joseph Misiti <joseph.misiti@gmail.com>
2015-08-20 19:06:51 -0400

    added more

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 citibike.json:
 	curl -o citibike.json 'https://www.citibikenyc.com/stations/json'
+
+201501-citibike-tripdata.zip:
+	curl -o  201501-citibike-tripdata.zip 'https://s3.amazonaws.com/tripdata/201501-citibike-tripdata.zip'
 	
 build-csv: citibike.json
 	cat citibike.json  | jq '.stationBeanList[]' | jq -r '[.latitude, .longitude, .stationName] | @csv' > citibike_stations.csv
+	
+build-trip: 201501-citibike-tripdata.zip
+	unzip 201501-citibike-tripdata.zip
+	cat 201501-citibike-tripdata.csv | head -n 10
+	rm 201501-citibike-tripdata.zip
 	
 clean:
 	rm *.json
---
+++ visualize-monthly-trips.R
@@ -0,0 +1,38 @@
+#
+#
+# Thanks
+# http://blog.dominodatalab.com/geographic-visualization-with-rs-ggmaps/
+#
+#
+
+library(ggmap)
+library(plyr)
+
+scaling_factor <- 10
+
+if(!exists("tripData")){
+  tripData <- read.csv("201501-citibike-tripdata.csv")
+}
+
+#print(summary(tripData))
+#print(names(tripData))
+
+trips = data.frame(tripData$start.station.latitude, 
+                   tripData$start.station.longitude, 
+                   tripData$start.station.id)
+
+colnames(trips) = c('lat','lon','stations') 
+trips$count <- 1
+trips.sum <- ddply(trips, c("lat","lon","stations"), summarize, count = sum(count))
+print(head(trips.sum))
+trips.sum$size <- scaling_factor*trips.sum$count / max(trips.sum$count)
+
+
+LES = as.numeric(geocode("Lower East Side"))
+MANHATTAN = ggmap(get_googlemap(center=LES, scale=1, zoom=13), extent="normal")
+MANHATTAN <- MANHATTAN + 
+  geom_point(aes(x=lon, y=lat), data=trips.sum, col="red", alpha=.5, size=trips.sum$size)
+print(MANHATTAN)
+
+
+
