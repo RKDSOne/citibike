88ec2616ec22e4e289e9d6528a48683beafd208e
Joseph Misiti <joseph.misiti@gmail.com>
2015-08-20 16:45:21 -0400

    added script

---
+++ visualize.R
@@ -0,0 +1,13 @@
+
+library(ggmap)
+
+data <- read.csv("citibike_stations.csv")
+colnames(data) = c('lat','lon','name')  
+
+usa_center = as.numeric(geocode("Lower East Side"))
+MANHATTAN = ggmap(get_googlemap(center=usa_center, scale=1, zoom=12), extent="normal")
+MANHATTAN <- MANHATTAN + 
+  geom_point(aes(x=lon, y=lat), data=data, col="red", alpha=.5, size=3)
+print(MANHATTAN)
+print("---- done -------")
+
