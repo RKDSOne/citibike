19a908fac96557e23b56e035e10d43efc70bf098
Joseph Misiti <joseph.misiti@gmail.com>
2015-08-20 20:54:32 -0400

    updated fonts

--- visualize-stations.R
+++ visualize-stations.R
@@ -8,7 +8,7 @@ usa_center = as.numeric(geocode("Lower East Side"))
 MANHATTAN = ggmap(get_googlemap(center=usa_center, scale=1, zoom=12), extent="normal")
 MANHATTAN <- MANHATTAN + 
   geom_point(aes(x=lon, y=lat), data=data, col="red", alpha=.5, size=3) + 
-  ggtitle("Citi Bike Stations in NYC")
+  ggtitle("Citi Bike Stations in NYC") + theme(plot.title=element_text(family="Times", face="bold", size=40))
 print(MANHATTAN)
 
 png(filename="output/manhattan.png",width=1000,height=1000)
--- visualize_gender_2.R
+++ visualize_gender_2.R
@@ -33,11 +33,13 @@ trips.sum$size <- scaling_factor*trips.sum$count / max(trips.sum$count)
 LES = as.numeric(geocode("Lower East Side"))
 MANHATTAN = ggmap(get_googlemap(center=LES, scale=1, zoom=12), extent="normal")
 MANHATTAN <- MANHATTAN + 
-  geom_point(aes(x=lon, y=lat), data=trips.sum, col="purple", alpha=.5, size=trips.sum$size)
+  geom_point(aes(x=lon, y=lat), data=trips.sum, col="purple", alpha=.5, size=trips.sum$size) +
+  ggtitle("Gender 2") + 
+  theme(plot.title=element_text(family="Times", face="bold", size=40))
 print(MANHATTAN)
 
 
-png(filename="gender_2.png",width=1000,height=1000)
+png(filename="output/gender_2.png",width=1000,height=1000)
 plot(MANHATTAN)
 dev.off()
 
